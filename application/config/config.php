90bb6733b3270b49bce96a7335b2d7fe74a3891a
unique211 <vishal.rkcet@gmail.com>
2019-12-05 18:15:57 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/chandrapur/';
+$config['base_url'] = 'https://chandrpur.herokuapp.com/';
 //$config['base_url'] = 'http://allysoftsolutions.com/chandrapur/';
 //$config['base_url'] = 'http://fe4f9466.ngrok.io/chandrapur/';
 /*
