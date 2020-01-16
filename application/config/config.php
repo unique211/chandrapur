723351f5c666c460a0b2ee69d862a0c1fb6f5c74
unique211 <vishal.rkcet@gmail.com>
2020-01-16 15:16:58 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,9 +23,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = 'https://chandrpur.herokuapp.com/';
+$config['base_url'] = 'https://chandrpur.herokuapp.com/';
 //$config['base_url'] = 'http://allysoftsolutions.com/chandrapur/';
-$config['base_url'] = 'http://localhost/chandrapur/';
+//$config['base_url'] = 'http://localhost/chandrapur/';
 /*
 |--------------------------------------------------------------------------
 | Index File
